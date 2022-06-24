16ce316899c550ad030a398891aff054bc2a40a7
cappittall <netcat16@gmail.com>
2022-06-24 09:07:20 +0300

    firstload

---
+++ dl_model.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# Download models
+# get id code of file from right click of file + share link + exctrac from share link
+
+#1 besas-new-model50-effdet00-max25-edgetpu.tflite
+#2 besas-new-model50-efdet02-edgetpu.tflite
+#3 besas-new-model50-effdet01-max25-edgetpu.tflite
+#4 besas-new-model50-effdet01-edgetpu-ver2.tflite
+#5 
+
+#wget --no-check-certificate \
+#'https://docs.google.com/uc?export=download&id=1-3FqGyPgkFBE2P3uXzQXpZDMRWVONKRu' \
+#-O /usr/share/edgetpudemo/besas-new-model50-effdet00-max25-edgetpu.tflite
+
+
+#wget --no-check-certificate \
+#'https://docs.google.com/uc?export=download&id=1-EaCvS5iTJBAms0SQjURCDaF8Q2bDyvY' \
+#-O /usr/share/edgetpudemo/besas-new-model50-efdet02-edgetpu.tflite
+
+#wget --no-check-certificate \
+#'https://docs.google.com/uc?export=download&id=1J2gSheWQjid33jfnqLLDV8CYrtRBB5Zv' \
+#-O /usr/share/edgetpudemo/besas-new-model50-effdet01-max25-edgetpu.tflite
+
+
+#wget --no-check-certificate \
+#'https://docs.google.com/uc?export=download&id=1h-k9d1qEjOiKRRtMvXC4kwmWoARtGNoo' \
+#-O /usr/share/edgetpudemo/besas-new-model50-effdet01-edgetpu-ver2.tflite
+
+
+
+# 1 besas-lite-model50-effdet00-max25-edgetpu.tflite
+# https://drive.google.com/file/d/1-3FqGyPgkFBE2P3uXzQXpZDMRWVONKRu/view?usp=sharing
+
+# 2 besas-lite-model50-efdet02-edgetpu.tflite
+# https://drive.google.com/file/d/1-EaCvS5iTJBAms0SQjURCDaF8Q2bDyvY/view?usp=sharing
+
+# 3 besas-lite-model50-effdet01-max25-edgetpu.tflite
+# https://drive.google.com/file/d/1J2gSheWQjid33jfnqLLDV8CYrtRBB5Zv/view?usp=sharing
+
+# 4 besas-lite-model50-effdet01-edgetpu-ver2.tflite
+# https://drive.google.com/file/d/1h-k9d1qEjOiKRRtMvXC4kwmWoARtGNoo/view?usp=sharing
+
+#5 efficientdet2-lite-ekmek-edgetpu.tflite 
+# https://drive.google.com/file/d/1YA5pJY3UP7hppE-hBXmvaO6LgasxNdj2/view?usp=sharing
+
+#6 all4one
+# https://drive.google.com/file/d/1a5NxQVQL4m7ig2Z1BvnEZF9G-rwrTwTN/view?usp=sharing
+
+wget --no-check-certificate \
+'https://docs.google.com/uc?export=download&id=1a5NxQVQL4m7ig2Z1BvnEZF9G-rwrTwTN' \
+-O /usr/share/edgetpudemo/all4one.tflite 
